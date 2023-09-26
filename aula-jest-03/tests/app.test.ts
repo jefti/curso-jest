d2fc75fef6cafab5bbb1da86cd76db6ceaa3844a
jefti <jeftimeira@gmail.com>
2023-09-26 16:12:23 -0300

    exercicio aula 03

---
+++ aula-jest-03/tests/app.test.ts
@@ -0,0 +1,14 @@
+describe("Exercicio da aula 03",()=>{
+    it("Soma",()=>{
+        expect(2+2).toBe(4);
+    });
+    it("Subtração",()=>{
+        expect(3-2).toBe(1);
+    });
+    it("Mutiplicação",()=>{
+        expect(2*2).toBe(4);
+    });
+    it("Divisão",()=>{
+        expect(2/2).toBe(1);
+    })  
+})
